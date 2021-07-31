3996475cc4c9e61e946c57819f710190c7b692e0
zlataovce <34477304+zlataovce@users.noreply.github.com>
2021-07-31 13:35:54 +0200

    unfinished

--- src/main/resources/static/js/view.js
+++ src/main/resources/static/js/view.js
@@ -21,13 +21,7 @@ function pickLine(elem) {
     scrollAndHighlight(elem);
 }
 
-window.onload = function () {
-    if (window.location.hash !== "") {
-        console.log("line hash found, scrolling to line " + window.location.hash.replace("#L", ""));
-        scrollAndHighlight(window.location.hash.replace("#", ""));
-    }
-
-    let paragraphs = document.getElementsByTagName("p");
+function termHighlight(paragraphs) {
     for (let i = 0; i < paragraphs.length; i++) {
         let elem = paragraphs[i];
         if (elem.innerHTML.includes("WARN")) {
@@ -55,8 +49,62 @@ window.onload = function () {
                 } else if (match.includes("37")) {
                     elem.style.setProperty("color", "white", "important");
                 }
+                elem.innerHTML = elem.innerHTML.replace(match, "<span class='hidden'>" + match + "</span>>")
             }
         }
-        elem.innerHTML = elem.innerHTML.replaceAll(/[\u001b\u009b][[()#;?]*(?:[0-9]{1,4}(?:;[0-9]{0,4})*)?[0-9A-ORZcf-nqry=><]/g, "");
+    }
+}
+
+function normalHighlight(paragraphs) {
+    let text = paragraphs.map(x => x.getElementsByTagName("span")[0].innerText).join('\n');
+    let lang = hljs.highlightAuto(text).lang;
+}
+
+function getOrDefault(item, def) {
+    let result = window.localStorage.getItem(item);
+
+    if (result == null) {
+        result = def;
+        window.localStorage.setItem(item, def);
+    }
+    return result;
+}
+
+function getParagraphs() {
+    let paragraphs = document.getElementsByTagName("p");
+    for (let p of paragraphs) {
+        if (!p.id.startsWith("L")) {
+            paragraphs = removeItemOnce(paragraphs, p);
+        }
+    }
+    return paragraphs;
+}
+
+function removeHighlights(paragraphs) {
+    for (let p of paragraphs) {
+        p.removeAttribute("color");
+        p.getElementsByTagName("span")[0].innerHTML = p.getElementsByTagName("span")[0].innerText;
+    }
+}
+
+function removeItemOnce(arr, value) {
+    let index = arr.indexOf(value);
+    if (index > -1) {
+        arr.splice(index, 1);
+    }
+    return arr;
+}
+
+window.onload = function () {
+    if (window.location.hash !== "") {
+        console.log("line hash found, scrolling to line " + window.location.hash.replace("#L", ""));
+        scrollAndHighlight(window.location.hash.replace("#", ""));
+    }
+
+    let paragraphs = getParagraphs();
+    if (getOrDefault("terminal", "false") === "true") {
+        termHighlight(paragraphs);
+    } else {
+
     }
 }
